6b7c481f3193958ec9305f864f123674dd3a7b43
Satyavrat <satyavratg.ug25.cs@nitp.ac.in>
2026-06-02 08:46:57 +0530

    This is the second commit

---
+++ hello.py
@@ -0,0 +1,7 @@
+print("Hello there, this file is gonna be uploaded to GitHub")
+print("This is the second line of code")
+print("This is the third line of code")
+print("This line was addded by Copilot" \
+"This line was added by me")
+print("This line was added after initial commit" \
+"This line was added by me  after initial commit")
